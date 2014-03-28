700c90605ac3f9e165a573b1d80f5137dc8c31cf
thebrettd <thebrettd@gmail.com>
2014-03-27 21:53:00 -0700

    spec tweak

--- specs/card_spec.rb
+++ specs/card_spec.rb
@@ -1,11 +1,11 @@
 require 'lib/blackjack/card'
 
-describe Card, '#intialize' do
+describe Card do
 
   it 'should have a suit and value' do
     card = Card.new(0,'Spade')
-    card.suit.nil?.should eq(false)
-    card.value.nil?.should eq(false)
+    card.suit.nil?.should be_false
+    card.value.nil?.should be_false
   end
 
 end
