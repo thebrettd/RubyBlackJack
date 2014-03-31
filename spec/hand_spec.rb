33173a2c55352a25a97582634a11fc55bfb49771
thebrettd <thebrettd@gmail.com>
2014-03-30 18:14:23 -0700

    Fix neglected tests

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -18,18 +18,18 @@ describe Hand do
   it 'adding a card should increase size' do
     hand = Hand.new
     card = Card.new(Suit::SPADE,Value::ACE)
-    hand.add_card(card)
+    hand.add_card(card, false)
     hand.size.should eq(1)
   end
 
   it 'only_two_cards? returns true for exactly 2 card hand' do
     hand = Hand.new
     card = Card.new(Suit::SPADE,Value::ACE)
-    hand.add_card(card)
+    hand.add_card(card, false)
     hand.only_two_cards?.should eq(false)
-    hand.add_card(card)
+    hand.add_card(card, false)
     hand.only_two_cards?.should eq(true)
-    hand.add_card(card)
+    hand.add_card(card, false)
     hand.only_two_cards?.should eq(false)
   end
 
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,53 +2,62 @@ require 'lib/player'
 require 'lib/card'
 require 'lib/suit'
 require 'lib/value'
+require 'lib/blackjack'
 
 describe Player do
 
   it 'has a name' do
-    player = Player.new('Test Player')
+    game = Blackjack.new(1)
+    player = Player.new('Test Player',game.shoe, game.dealer)
     player.name.should eq('Test Player')
   end
 
   it 'has a bankroll' do
-    player = Player.new('Test Player')
+    game = Blackjack.new(1)
+    player = Player.new('Test Player',game.shoe, game.dealer)
     player.bankroll.should eq(1000)
   end
 
   it 'should have an empty hand to start' do
-    player = Player.new('Test Player')
-    player.hands[0].cards.length.should eq(0)
+    game = Blackjack.new(1)
+    player = Player.new('Test Player',game.shoe, game.dealer)
+    player.hands.length.should eq(0)
   end
 
   it 'should be able to start a new empty hand' do
-    player = Player.new('Test Player')
+    game = Blackjack.new(1)
+    player = Player.new('Test Player',game.shoe, game.dealer)
     player.new_hands
     player.hands.nil?.should eq(false)
     player.hands[0].cards.size.should eq(0)
   end
 
   it 'should have a nil wager' do
-    player = Player.new('Test Player')
+    game = Blackjack.new(1)
+    player = Player.new('Test Player',game.shoe, game.dealer)
     player.current_wager.nil?.should eq(true)
   end
 
   it 'should have a non-nil wager after placing wagering' do
-    player = Player.new('Test Player')
+    game = Blackjack.new(1)
+    player = Player.new('Test Player',game.shoe, game.dealer)
     player.place_wager(5)
     player.current_wager.should eq(5)
   end
 
   it 'bankroll should decrease after placing wagering' do
-    player = Player.new('Test Player')
+    game = Blackjack.new(1)
+    player = Player.new('Test Player',game.shoe, game.dealer)
     player.place_wager(5)
     player.bankroll.should eq(995)
   end
 
   it 'hand size should increase after hitting' do
-    player = Player.new('Test Player')
+    game = Blackjack.new(1)
+    player = Player.new('Test Player',game.shoe, game.dealer)
     player.new_hands
 
-    player.hands[0].hit(Card.new(Suit::SPADE, Value::TEN))
+    player.hands[0].add_card(Card.new(Suit::SPADE, Value::TEN), false)
     player.hands[0].cards.size.should eq(1)
   end
 
